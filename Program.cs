e27df36fa54098c2be22f1fd5d9fa9b5de83909e
shevelan <shevelan@mail.ru>
2025-03-18 15:05:21 +0300

    ДЗ7

--- Program.cs
+++ Program.cs
@@ -1,128 +1,140 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Text.Json;
+using Newtonsoft.Json;
 
 public class F
 {
-    public int i1, i2, i3, i4, i5;
-
-    public F Get() => new F() { i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 };
+    public int i1;
+    public int i2;
+    public int i3;
+    public int i4;
+    public int i5;
 }
 
-public class Program
+public class SerializationBenchmark
 {
-    private const int Iterations = 10000;
-
     public static void Main(string[] args)
     {
-        F obj = new F().Get();
+        int iterations = 1000;
 
-        // 1. Кастомная сериализация свойств/полей
-        TimeSpan customSerializationTime = MeasureTime(() =>
-        {
-            for (int i = 0; i < Iterations; i++)
-            {
-                CustomSerialize(obj);
-            }
-        });
+        F obj = new F { i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 };
 
-        string serializedString = CustomSerialize(obj);
-        Console.WriteLine($"Custom Serialization Result: {serializedString}");
-        Console.WriteLine($"Custom Serialization Time ({Iterations} iterations): {customSerializationTime}");
+        // Рефлексия
+        (long reflectionSerializationTime, long reflectionDeserializationTime) =
+            BenchmarkReflection(obj, iterations);
 
-        // 2. Замер времени вывода в консоль
-        TimeSpan consoleOutputTime = MeasureTime(() => Console.WriteLine($"Console Output (from Custom Serialization): {serializedString}"));
-        Console.WriteLine($"Console Output Time: {consoleOutputTime}");
+        // Newtonsoft.Json
+        (long jsonSerializationTime, long jsonDeserializationTime) =
+            BenchmarkNewtonsoftJson(obj, iterations);
 
-        // 3. JSON сериализация
-        TimeSpan jsonSerializationTime = MeasureTime(() =>
-        {
-            for (int i = 0; i < Iterations; i++)
-            {
-                JsonSerialize(obj);
-            }
-        });
+        // Формирование результата для отправки
+        string result = $@"
+Сериализуемый класс: class F {{ int i1, i2, i3, i4, i5;}}
+код сериализации-десериализации: 
+количество замеров: {iterations} итераций
+мой рефлекшен:
+Время на сериализацию = {reflectionSerializationTime} мс
+Время на десериализацию = {reflectionDeserializationTime} мс
+стандартный механизм (NewtonsoftJson):
+Время на сериализацию = {jsonSerializationTime} мс
+Время на десериализацию = {jsonDeserializationTime} мс";
 
-        string jsonString = JsonSerialize(obj);
-        Console.WriteLine($"JSON Serialization Result: {jsonString}");
-        Console.WriteLine($"JSON Serialization Time ({Iterations} iterations): {jsonSerializationTime}");
+        Console.WriteLine(result);
 
-        // 4. Десериализация из строки (INI-подобный формат)
-        TimeSpan customDeserializationTime = MeasureTime(() =>
-        {
-            for (int i = 0; i < Iterations; i++)
-            {
-                CustomDeserialize(serializedString);
-            }
-        });
-
-        F deserializedObj = CustomDeserialize(serializedString);
-        Console.WriteLine($"Custom Deserialization Result: i1={deserializedObj.i1}, i2={deserializedObj.i2}, i3={deserializedObj.i3}, i4={deserializedObj.i4}, i5={deserializedObj.i5}");
-        Console.WriteLine($"Custom Deserialization Time ({Iterations} iterations): {customDeserializationTime}");
-
+        // Здесь можно добавить код для отправки результата в чат
+        // Например, через какой-либо API или библиотеку
 
         Console.ReadKey();
     }
 
-    // Кастомная сериализация
-    public static string CustomSerialize(F obj)
+    static (long, long) BenchmarkReflection(F obj, int iterations)
     {
-        StringBuilder sb = new StringBuilder();
-        Type type = typeof(F);
-        FieldInfo[] fields = type.GetFields();
+        Stopwatch swSerialization = Stopwatch.StartNew();
+        string serializedData = "";
 
-        foreach (FieldInfo field in fields)
+        for (int i = 0; i < iterations; i++)
         {
-            sb.Append($"{field.Name}={field.GetValue(obj)};");
+            serializedData = SerializeReflection(obj);
         }
 
-        return sb.ToString();
+        swSerialization.Stop();
+        long serializationTime = swSerialization.ElapsedMilliseconds;
+
+        Stopwatch swDeserialization = Stopwatch.StartNew();
+        F deserializedObj;
+
+        for (int i = 0; i < iterations; i++)
+        {
+            deserializedObj = DeserializeReflection(serializedData);
+        }
+
+        swDeserialization.Stop();
+        long deserializationTime = swDeserialization.ElapsedMilliseconds;
+
+        return (serializationTime, deserializationTime);
     }
 
-    // Кастомная десериализация (INI-подобный формат)
-    public static F CustomDeserialize(string serializedString)
+    static (long, long) BenchmarkNewtonsoftJson(F obj, int iterations)
+    {
+        Stopwatch swSerialization = Stopwatch.StartNew();
+        string serializedData = "";
+
+        for (int i = 0; i < iterations; i++)
+        {
+            serializedData = JsonConvert.SerializeObject(obj);
+        }
+
+        swSerialization.Stop();
+        long serializationTime = swSerialization.ElapsedMilliseconds;
+
+        Stopwatch swDeserialization = Stopwatch.StartNew();
+        F deserializedObj;
+
+        for (int i = 0; i < iterations; i++)
+        {
+            deserializedObj = JsonConvert.DeserializeObject<F>(serializedData);
+        }
+
+        swDeserialization.Stop();
+        long deserializationTime = swDeserialization.ElapsedMilliseconds;
+
+        return (serializationTime, deserializationTime);
+    }
+
+    static string SerializeReflection(F obj)
+    {
+        Type type = typeof(F);
+        string result = "";
+
+        foreach (FieldInfo field in type.GetFields())
+        {
+            result += $"{field.Name}:{field.GetValue(obj)};";
+        }
+
+        return result;
+    }
+
+    static F DeserializeReflection(string data)
     {
         F obj = new F();
-        string[] parts = serializedString.Split(';', StringSplitOptions.RemoveEmptyEntries);
+        Type type = typeof(F);
+
+        string[] parts = data.Split(';', StringSplitOptions.RemoveEmptyEntries);
 
         foreach (string part in parts)
         {
-            string[] keyValue = part.Split('=');
-            if (keyValue.Length == 2)
-            {
-                string fieldName = keyValue[0];
-                string fieldValue = keyValue[1];
+            string[] keyValue = part.Split(':');
+            string fieldName = keyValue[0];
+            string fieldValue = keyValue[1];
 
-                FieldInfo field = typeof(F).GetField(fieldName);
-                if (field != null && field.FieldType == typeof(int))
-                {
-                    if (int.TryParse(fieldValue, out int parsedValue))
-                    {
-                        field.SetValue(obj, parsedValue);
-                    }
-                }
+            FieldInfo field = type.GetField(fieldName);
+            if (field != null)
+            {
+                field.SetValue(obj, Convert.ChangeType(fieldValue, field.FieldType));
             }
         }
 
         return obj;
-    }
-
-    // JSON сериализация
-    public static string JsonSerialize(F obj)
-    {
-        return JsonSerializer.Serialize(obj);
-    }
-
-    // Метод для замера времени
-    public static TimeSpan MeasureTime(Action action)
-    {
-        Stopwatch stopwatch = Stopwatch.StartNew();
-        action();
-        stopwatch.Stop();
-        return stopwatch.Elapsed;
     }
 }
